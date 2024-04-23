8dff4dcab3a0852cb3f9bd25d098e7daa5b10f0c
Saad0095 <saadkhalid0095@gmail.com>
2024-04-23 17:04:35 +0500

    Added Projects and Skills Section

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ var typed = new Typed('#element', {
     strings: ['', 'a Web Developer'],
     typeSpeed: 100,
 });
+
+/* */
+
+document.querySelectorAll('.progress-bar').forEach(bar => {
+    bar.addEventListener('mouseenter', function() {
+        this.style.width = '100%';
+    });
+
+    // bar.addEventListener('mouseleave', function() {
+    //     this.style.width = "0%";
+    // });
+});
