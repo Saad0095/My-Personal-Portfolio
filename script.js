cde9e596d0370e8c3b1835fa2390d0020b6af375
Saad0095 <saadkhalid0095@gmail.com>
2024-05-08 23:08:33 +0500

    Fixed Navbar and color

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 /* Setup and start animation! */
- 
+
 var typed = new Typed('#element', {
     strings: ['', 'a Web Developer'],
     typeSpeed: 100,
 });
 
-/* */
+/* */ 
+
+let navItem = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse")
+navItem.forEach((a) => {
+    a.addEventListener("click", () => {
+        navCollapse.classList.remove("show");
+    })
+})
+
+let nav = document.querySelector(".navbar");
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 20){
+        nav.classList.add("navbar-scrolled")
+    }
+    else{
+        nav.classList.remove("navbar-scrolled")
+    }
+}
 
 document.querySelectorAll('.progress').forEach(bar => {
-    bar.addEventListener('mouseenter', function() {
+    bar.addEventListener('mouseenter', function () {
         this.querySelector('.progress-bar').style.width = "100%";
     });
 });
+
+
