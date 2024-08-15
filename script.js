61fa96851c213f67a04e265fb58d34c7e415160f
Saad0095 <saadbinkhalid1895@gmail.com>
2024-08-15 11:13:20 -0700

    Made some changes in styling and added horizontal scrolling

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
 let navItem = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelector(".navbar-collapse")
+let navCollapse = document.querySelector(".navbar-collapse");
 
 navItem.forEach((a) => {
-    a.addEventListener("click", () => {
-        navCollapse.classList.remove("show");
-    })
-})
+  a.addEventListener("click", () => {
+    navCollapse.classList.remove("show");
+  });
+});
 
 let nav = document.querySelector(".navbar");
 window.onscroll = () => {
-    if (document.documentElement.scrollTop > 20){
-        nav.classList.add("navbar-scrolled")
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("navbar-scrolled");
+  } else {
+    nav.classList.remove("navbar-scrolled");
+  }
+};
+
+let scrollContainer = document.querySelector(".scroll-container");
+let scrollAmount = 0;
+let scrollStep = 2;
+let maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+let leftBtn = document.querySelector(".left-button");
+let rightBtn = document.querySelector(".right-button");
+
+
+
+let autoScrollInterval;
+
+setTimeout(() => {
+  autoScrollInterval = setInterval(autoScroll, 10);
+}, 3000);
+
+function autoScroll() {
+  scrollAmount += scrollStep;
+  scrollContainer.scrollLeft = scrollAmount;
+
+  setTimeout(() => {
+    if (scrollAmount >= maxScroll || scrollAmount <= 0) {
+      scrollStep = -scrollStep;
     }
-    else{
-        nav.classList.remove("navbar-scrolled")
-    }
+  }, 500);
 }
 
-let scrollContainer = document.querySelector('.scroll-container')
-let leftBtn = document.querySelector('.left-button')
-let rightBtn = document.querySelector('.right-button')
 
-leftBtn.addEventListener("click", ()=>{
-    scrollContainer.scrollBy({
-        left:300,
-        behavior:"smooth"
-    })
-})
+leftBtn.addEventListener("click", () => {
+  clearInterval(autoScrollInterval);
 
-leftBtn.addEventListener("click", ()=>{
-    scrollContainer.scrollBy({
-        left:-400,
-        behavior:"smooth"
-    })
-})
+  scrollContainer.scrollBy({
+    left: -400,
+    behavior: "smooth",
+  });
+});
 
-rightBtn.addEventListener("click", ()=>{
-    scrollContainer.scrollBy({
-        left:400,
-        behavior:"smooth"
-    })
-})
+rightBtn.addEventListener("click", () => {
+  clearInterval(autoScrollInterval);
 
-
-
-
+  scrollContainer.scrollBy({
+    left: 400,
+    behavior: "smooth",
+  });
+});
